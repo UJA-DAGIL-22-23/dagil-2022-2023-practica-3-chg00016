8d20d0dea90a12b35ebf107334196771adeddf4d
Carmen Huesa Guardiola <chg00016@red.ujaen.es>
2023-04-18 16:57:09 +0200

    funcionalidad 01 terminada

--- ms-plantilla/callbacks.js
+++ ms-plantilla/callbacks.js
@@ -93,9 +93,9 @@ const CB_OTHERS = {
         try {
             CORS(res).status(200).json({
                 mensaje: "Microservicio MS Plantilla: acerca de",
-                autor: "¿¿¿ AUTOR ???",
-                email: "¿¿¿ EMAIL ???",
-                fecha: "¿¿¿ FECHA ???"
+                autor: "Carmen Huesa Guardiola",
+                email: "chg00015@red.ujaen.es",
+                fecha: "18-04-2023"
             });
         } catch (error) {
             CORS(res).status(500).json({ error: error.description })
