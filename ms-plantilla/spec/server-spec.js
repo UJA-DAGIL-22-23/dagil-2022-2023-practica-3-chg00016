83b9401e9ff0b271ca19d26adc210222c019ac6e
Carmen Huesa Guardiola <chg00016@red.ujaen.es>
2023-04-18 16:54:41 +0200

    Spec de funcionalidad 01

--- ms-plantilla/spec/server-spec.js
+++ ms-plantilla/spec/server-spec.js
@@ -39,6 +39,9 @@ describe('Servidor PLANTILLA:', () => {
           //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
           assert(res.body.hasOwnProperty('mensaje'));
           assert(res.body.mensaje === "Microservicio MS Plantilla: acerca de");
+          assert(res.body.autor == "Carmen Huesa Guardiola");
+          assert(res.body.email == "chg00016@red.ujaen.es");
+          assert(res.body.fecha == "18-04-2023");
 
         })
         .end((error) => { error ? done.fail(error) : done() })
